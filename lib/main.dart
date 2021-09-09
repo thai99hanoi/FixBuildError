54fed443a7e8676b60a246332f624d2389d45e2b
Phung Dat <datpthe130350@fpt.edu.vn>
2021-09-09 22:05:35 +0700

    commit nay`

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: LoginPage()
+    );
+  }
+
+}
