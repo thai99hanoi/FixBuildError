28cac4e2409839471dd6c13e1c9b338e552cbb0a
Phung Dat <datpthe130350@fpt.edu.vn>
2021-09-12 13:38:13 +0700

    update login

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -6,102 +6,158 @@ class LoginPage extends StatefulWidget {
 }
 
 class _State extends State<LoginPage> {
+  bool _showPass = false;
+  TextEditingController _userController = new TextEditingController();
+  TextEditingController _passController = new TextEditingController();
+  var _userNameErr = "Username is invalid!";
+  var _passNameErr = "Password must more than 6 characters";
+  var _userInvalid = false;
+  var _passInvalid = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-      constraints: BoxConstraints.expand(),
-      color: Colors.cyan[200],
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-            child: SizedBox(
-              height: 155,
-              child: Image.asset("assets/images/logo.png", fit: BoxFit.contain,),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(30, 100, 30, 0),
+        // constraints: BoxConstraints.expand(),
+        // color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: SizedBox.fromSize(
+                child: Image.asset(
+                  "assets/images/logo.png",
+                  fit: BoxFit.contain,
+                  height: 130,
+                  width: 130,
+                ),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-            child: Text(
-              "Health Care System",
-              style: TextStyle(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
+              child: Text(
+                "Health Care System\nFor COVID Patients At Home",
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
-                  fontSize: 30),
+                  fontSize: 25,
+                ),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-            child: TextField(
-              style: TextStyle(fontSize: 18, color: Colors.black),
-              decoration: InputDecoration(
-                  labelText: "Tên Đăng Nhập",
-                  labelStyle:
-                      TextStyle(color: Color(0xff888888), fontSize: 15)),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: TextField(
+                controller: _userController,
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                decoration: InputDecoration(
+                    labelText: "USERNAME",
+                    errorText: _userInvalid ? _userNameErr : null,
+                    labelStyle:
+                        TextStyle(color: Color(0xff888888), fontSize: 15)),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-            child: Stack(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: Stack(
                 alignment: AlignmentDirectional.centerEnd,
                 children: <Widget>[
                   TextField(
+                    controller: _passController,
                     style: TextStyle(fontSize: 18, color: Colors.black),
-                    obscureText: true,
+                    obscureText: !_showPass,
                     decoration: InputDecoration(
-                        labelText: "Nhập Mật Khẩu",
+                        labelText: "PASSWORD",
+                        errorText: _passInvalid ? _passNameErr : null,
                         labelStyle:
                             TextStyle(color: Color(0xff888888), fontSize: 15)),
                   ),
-                  Text(
-                    "Show",
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 13,
-                        fontWeight: FontWeight.bold),
+                  GestureDetector(
+                    onTap: onToggleShowPass,
+                    child: Text(
+                      _showPass ? "HIDE" : "SHOW",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold),
+                    ),
                   )
-                ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-            child: SizedBox(
-              width: double.infinity,
-              height: 56,
-              child: RaisedButton(
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8))),
-                onPressed: onSignInClicked,
-                child: Text("Đăng nhập"),
+                ],
               ),
             ),
-          ),
-          Container(
-            height: 138,
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Quên Mật Khẩu",
-                  style: TextStyle(fontSize: 15, color: Colors.blue),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
+                  onPressed: onSignInClicked,
+                  child: Text(
+                    "SIGN IN",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
                 ),
-              ],
+              ),
             ),
-          )
-        ],
+            Container(
+              height: 130,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "NEW USER? SIGN UP",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff888888),
+                    ),
+                  ),
+                  Text(
+                    "FORGOT PASSWORD?",
+                    style: TextStyle(fontSize: 15, color: Colors.blue),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
-  void onSignInClicked() {
-    setState(() {
 
+  void onToggleShowPass() {
+    setState(() {
+      _showPass = !_showPass;
     });
   }
+
+  void onSignInClicked() {
+    setState(() {
+      if (_userController.text.length < 6 ||
+          !_userController.text.contains("@")) {
+        _userInvalid = true;
+      } else {
+        _userInvalid = false;
+      }
+
+      if (_passController.text.length < 6) {
+        _passInvalid = true;
+      } else {
+        _passInvalid = false;
+      }
+
+      if (!_userInvalid && !_passInvalid) {
+        // Navigator.push(context, MaterialPageRoute(builder: gotoHome));
+      }
+    });
+  }
+// Widget gotoHome(BuildContext context){
+//   return HomePage();
+// }
 }
-
-
