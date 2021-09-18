d51c2be92c146854afb992babd283bff07bce581
Trịnh Nam Thái <thai99hanoi@gmail.com>
2021-09-18 14:47:42 +0700

    Home Screen Design

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:provider/provider.dart';
 import 'package:heath_care/ui/login_screen.dart';
@@ -8,22 +9,215 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.of(context).pushReplacementNamed("/");
-            Provider.of<Auth>(context, listen: false).logout();
-          },
-          shape:
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          child: Text("Logout"),
-          color: Colors.green,
+        body: ListView(
+      children: <Widget>[
+        Container(
+          alignment: AlignmentDirectional.centerStart,
+          padding: const EdgeInsets.fromLTRB(10, 0, 0, 20),
+          child: Row(
+            children: [
+              SizedBox.fromSize(
+                child: Image.asset(
+                  "assets/images/img.png",
+                  fit: BoxFit.contain,
+                  width: 130.0,
+                  height: 125.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: SizedBox.fromSize(
+                    child: TextButton(
+                        onPressed: null,
+                        child: Image.asset(
+                          'assets/images/img_2.png',
+                          fit: BoxFit.contain,
+                          height: 21,
+                          width: 21,
+                        ))),
+              ),
+              SizedBox.fromSize(
+                child: Image.asset(
+                  "assets/images/img_1.png",
+                  fit: BoxFit.contain,
+                  height: 53,
+                  width: 53,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        Container(
+          alignment: AlignmentDirectional.center,
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: Text(
+            'HƯỚNG DẪN \n SỬ DỤNG PHẦN MỀM',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.teal[500],
+              fontSize: 25,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(60, 30, 60, 0),
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  child: SizedBox(
+                    width: 130.0,
+                    height: 125.0,
+                    child: new RaisedButton(
+                      color: Color.fromRGBO(78, 159, 193, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      onPressed: () {},
+                      child: new Text(
+                        'Khai Báo Y Tế',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: SizedBox(
+                    width: 130.0,
+                    height: 125.0,
+                    child: new RaisedButton(
+                      color: Color.fromRGBO(78, 159, 193, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      onPressed: () {},
+                      child: new Text(
+                        'Báo Cáo \n Tình Trạng Sức Khoẻ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(60, 20, 60, 0),
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: SizedBox(
+                    width: 130.0,
+                    height: 125.0,
+                    child: new RaisedButton(
+                      color: Color.fromRGBO(78, 159, 193, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      onPressed: () {},
+                      child: new Text(
+                        'Khai Báo y Tế',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: SizedBox(
+                    width: 130.0,
+                    height: 125.0,
+                    child: new RaisedButton(
+                      color: Color.fromRGBO(78, 159, 193, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      onPressed: () {},
+                      child: new Text(
+                        'Báo Cáo Tình Trạng Sức Khoẻ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(60, 20, 60, 0),
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: SizedBox(
+                    width: 130.0,
+                    height: 125.0,
+                    child: new RaisedButton(
+                      color: Color.fromRGBO(78, 159, 193, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      onPressed: () {},
+                      child: new Text(
+                        'Khai Báo y Tế',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: SizedBox(
+                    width: 130.0,
+                    height: 125.0,
+                    child: new RaisedButton(
+                      color: Color.fromRGBO(78, 159, 193, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      onPressed: () {},
+                      child: new Text(
+                        'Báo Cáo Tình Trạng Sức Khoẻ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
