b2c2954d9fe1eadc18d17d84079c7198fba04231
Phung Dat <datpthe130350@fpt.edu.vn>
2021-09-22 23:40:53 +0700

    add repository getUserOnline

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -8,4 +8,14 @@ class UserRepository{
     var entriesList = response.entries.toList();
     return User.fromMap(entriesList[1].value);
   }
+
+  Future<List<User>?> getUserOnline(userId) async {
+    List<User>? userOnline;
+    Map<String, dynamic> response = await apiBaseHelper.get("/api/v1/users-online"+userId);
+    var entriesList = response.entries.toList();
+    for(User user in entriesList[1].value){
+      userOnline!.add(user);
+    }
+    return userOnline;
+  }
 }
