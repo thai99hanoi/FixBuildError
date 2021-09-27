5c0630f15ec37eb44b3e3621d3a9a42626e7ecc4
Trịnh Nam Thái <thai99hanoi@gmail.com>
2021-09-27 21:41:02 +0700

    New Home Page

---
+++ lib/ui/components/NavSideBar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Side menu',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Welcome'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Profile'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.border_color),
+            title: Text('Feedback'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {},
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/new_home.dart
+++ lib/ui/new_home.dart
@@ -1,9 +1,171 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'components/Bottom_Navigator.dart';
+import 'components/NavSideBar.dart';
 
 // ignore: camel_case_types
 class homeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+        appBar: AppBar(title: Text("Trang Chủ")),
+        bottomNavigationBar: BottomNavigator(),
+        body: ListView(children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text("TÌNH HÌNH DỊCH TẠI VIỆT NAM",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      child: Column(children: [
+                        Container(
+                          height: 70,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Color.fromRGBO(243, 231, 189, 1),
+                          ),
+                          child: Column(children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text("667.023",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: Color.fromRGBO(192, 150, 0, 1))),
+                            ),
+                            Text("Ca Nhiễm",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 10),
+                                textAlign: TextAlign.center),
+                          ]),
+                        ),
+                        Container(
+                            height: 40,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                color: Color.fromRGBO(232, 200, 89, 1)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                "+9.373",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white,
+                                    fontSize: 10),
+                                textAlign: TextAlign.center,
+                              ),
+                            ))
+                      ]),
+                    ),
+                  ),
+                  //One Column
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      child: Column(children: [
+                        Container(
+                          height: 70,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Color.fromRGBO(243, 189, 189, 1),
+                          ),
+                          child: Column(children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text("667.023",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: Color.fromRGBO(232, 89, 89, 1))),
+                            ),
+                            Text("Ca Nhiễm",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 10),
+                                textAlign: TextAlign.center),
+                          ]),
+                        ),
+                        Container(
+                            height: 40,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                color: Color.fromRGBO(232, 89, 89, 1)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                "+9.373",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white,
+                                    fontSize: 10),
+                                textAlign: TextAlign.center,
+                              ),
+                            ))
+                      ]),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      child: Column(children: [
+                        Container(
+                          height: 70,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Color.fromRGBO(189, 243, 194, 1),
+                          ),
+                          child: Column(children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text("667.023",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: Color.fromRGBO(0, 192, 8, 1))),
+                            ),
+                            Text("Ca Nhiễm",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 10),
+                                textAlign: TextAlign.center),
+                          ]),
+                        ),
+                        Container(
+                            height: 40,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                color: Color.fromRGBO(0, 192, 8, 1)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                "+9.373",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white,
+                                    fontSize: 10),
+                                textAlign: TextAlign.center,
+                              ),
+                            ))
+                      ]),
+                    ),
+                  ),
+                ]),
+          ),
+        ]),
+        drawer: NavDrawer());
   }
 }
