f333c2b2db4b7f7a2309c10df7c4f796bc85431d
Trịnh Nam Thái <thai99hanoi@gmail.com>
2021-10-29 17:52:59 +0700

    Add Bool isCheck

--- lib/model/exercise.dart
+++ lib/model/exercise.dart
@@ -3,13 +3,13 @@ class Exercise {
   String? name;
   String? description;
   String? thumbnail;
+  bool isCheck = false;
 
   Exercise({this.id, this.name, this.description, this.thumbnail});
 
-  factory Exercise.fromJson(Map<String, dynamic> json)=> Exercise(
+  factory Exercise.fromJson(Map<String, dynamic> json) => Exercise(
       id: json['id'] as int,
       name: json['name'] as String,
       description: json['description'] as String,
-      thumbnail: json['thumbnail'] as String
-  );
+      thumbnail: json['thumbnail'] as String);
 }
--- lib/model/medicine.dart
+++ lib/model/medicine.dart
@@ -3,13 +3,13 @@ class Medicine {
   String? name;
   String? description;
   String? thumbnail;
+  bool isCheck = false;
 
   Medicine({this.id, this.name, this.description, this.thumbnail});
 
-  factory Medicine.fromJson(Map<String, dynamic> json)=> Medicine(
-    id: json['id'] as int,
-    name: json['name'] as String,
-    description: json['description'] as String,
-    thumbnail: json['thumbnail'] as String
-  );
+  factory Medicine.fromJson(Map<String, dynamic> json) => Medicine(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      description: json['description'] as String,
+      thumbnail: json['thumbnail'] as String);
 }
