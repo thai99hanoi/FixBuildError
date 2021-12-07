fc048d4fad0848ea1393d3d2f96744bb4e8b9dd8
Phung Dat <datpthe130350@fpt.edu.vn>
2021-12-08 04:40:19 +0700

    update model medicine

--- lib/model/medicine.dart
+++ lib/model/medicine.dart
@@ -2,14 +2,16 @@ class Medicine {
   int? id;
   String? name;
   String? description;
+  String? detail;
   String? thumbnail;
   bool isCheck = false;
 
-  Medicine({this.id, this.name, this.description, this.thumbnail});
+  Medicine({this.id, this.name, this.description, this.detail, this.thumbnail});
 
   factory Medicine.fromJson(Map<String, dynamic> json) => Medicine(
       id: json['id'] as int?,
       name: json['name'] as String?,
       description: json['description'] as String?,
+      detail: json['detail'] as String?,
       thumbnail: json['thumbnail'] as String?);
 }
